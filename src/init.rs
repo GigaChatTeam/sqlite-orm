1cf947550ca986e36939cbd5f9c7c52bb608b43b
justanothercatgirl <sotov2070@gmail.com>
2023-12-08 22:05:23 +0300

    Added rusqlite crate & Tested mods

---
+++ src/init.rs
@@ -0,0 +1,4 @@
+
+pub mod init {
+    pub const x: i32 = 69;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
 
+mod init;
+mod debug;
 
+use init::init::*;
+
+#[cfg(test)]
+pub mod testing {
+    #[test]
+    fn main() {
+//        use x;
+        println!("test launched!");
+        assert_eq!(2+2, 4);
+        assert_eq!(x, 69);
+    }   
+}
 
