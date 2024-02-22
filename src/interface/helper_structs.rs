e8782f236faa6a4bc9ab1621ce9ad1b06a117471
justanothercatgirl <sotov2070@gmail.com>
2024-02-23 00:07:32 +0300

    added licensing

--- src/interface/helper_structs.rs
+++ src/interface/helper_structs.rs
@@ -1,3 +1,24 @@
+/* ----------------------------------------------------------------------------
+   gigachat-orm - a library for caching and managing gigachat files
+   Copyright (C) 2024 Sotov Konstantin A
+
+   This file is part of gigachat-orm library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation; either version 3 of
+   the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.   
+   ----------------------------------------------------------------------------
+*/
+
 
 use microserde::{Serialize, Deserialize};
 
